a306f9d72f7ac77c7ad598021b301035f0fd8fcd
MathiasReker <mathias@reker.dk>
2018-12-21 02:14:29 +0100

    Braces

--- upgrade/upgrade-from-blockcontact.php
+++ upgrade/upgrade-from-blockcontact.php
@@ -1,10 +1,11 @@
 <?php
 
-if ( ! defined('_PS_VERSION_'))
-	exit;
+if ( ! defined('_PS_VERSION_')) {
+    exit;
+}
 
 function upgrade_module_2_0($object)
 {
-	return Configuration::deleteByName('BLOCKCONTACT_TELNUMBER')
+    return Configuration::deleteByName('BLOCKCONTACT_TELNUMBER')
         && Configuration::deleteByName('BLOCKCONTACT_EMAIL');
 }
